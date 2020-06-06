4d1445953b327bd661581da0872f51cbca5d708a
ongaaron <ong.aaron96@gmail.com>
2020-06-06 16:46:58 +0800

    trying travis

--- src/test/java/SUTTest.java
+++ src/test/java/SUTTest.java
@@ -1,8 +1,18 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
 public class SUTTest {
+	@Test
+	public void testStringConcatenation_length0_typeI() {
+		String s1 = "";
+        String s2 = "";
+        SUT sut = new SUT();
+        int result = sut.stringConcatenation(s1, s2);
+        assertEquals(result, 1);
+	}
+	
     @Test
     public void testStringConcatenation_length2_typeI() {
         String s1 = "a";
@@ -19,5 +29,21 @@ public class SUTTest {
         SUT sut = new SUT();
         int result = sut.stringConcatenation(s1, s2);
         assertEquals(result, 2);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testStringConcatenation_length40_throwsIllegalArgumentException1() {
+    	String s1 = "aaaaabbbbbcccccddddd";
+        String s2 = "eeeeefffffggggghhhhh";
+        SUT sut = new SUT();
+        sut.stringConcatenation(s1,  s2);
+    }
+    
+    @Test
+    public void testStringConcatenation_length40_throwsIllegalArgumentException2() {
+    	String s1 = "aaaaabbbbbcccccddddd";
+        String s2 = "eeeeefffffggggghhhhh";
+        SUT sut = new SUT();
+        assertThrows(IllegalArgumentException.class, () -> sut.stringConcatenation(s1,  s2));
     }
 }
